f8bff1e079d55da8e7fbad0001731330f0991b3f
Nguyễn Văn Trúc <vantruc572003@gmail.com>
2023-12-12 23:27:13 +0700

    update

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS  # Import the CORS module
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'Desgin-E.png')
+@app.route('/')
+def index():
+    return "Hello, Python!"
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
