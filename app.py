244332c99a5595018f469c5fbf8038bdebdb6730
Nguyễn Văn Trúc <vantruc572003@gmail.com>
2024-01-20 19:53:13 +0700

    up

--- app.py
+++ app.py
@@ -1,21 +1,96 @@
-from flask import Flask, jsonify, send_from_directory
+from process import run_img_to_text, run_autoclick, run_remove_bg
+
+from flask import Flask, jsonify, send_from_directory, request, render_template
 from flask_cors import CORS  # Import the CORS module
 import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.icon', mimetypes='Desgin-E.png')
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello, Python!"
+    result = None
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            filename = 'text.png'
+            file_path = os.path.join('uploads', filename)
+            uploaded_file.save(file_path)
+            print(file_path)
+            result = run_img_to_text(file_path)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'Hello from the backend Python! NVT'}
+    return render_template('index.html', result=result)
+
+
+def process_input(user_input):
+    return user_input
+
+
+@app.route('/api/img_to_text', methods=['GET', 'POST'])
+def img_to_text():
+    data = {
+        "code": 200,
+        "message": "Vui long nhap du lieu"
+    }
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            filename = 'text.png'
+            file_path = os.path.join('uploads', filename)
+            uploaded_file.save(file_path)
+            message = run_img_to_text(file_path)
+            data = {
+                "code": 200,
+                "message": message
+            }
     return jsonify(data)
 
+
+@app.route('/api/remove_bg', methods=['GET', 'POST'])
+def remove_bg():
+    data = {
+        "code": 200,
+        "message": "Vui long nhap du lieu"
+    }
+    if request.method == 'POST':
+        uploaded_file = request.files['img']
+        if uploaded_file.filename != '':
+            filename = 'removebg.png'
+            file_path = os.path.join('uploads', filename)
+            # full_path = os.path.join(app.root_path, file_path)
+            uploaded_file.save(file_path)
+            run_remove_bg(file_path)
+            data = {
+                "code": 200,
+                "url_img": request.url_root + file_path
+            }
+    return jsonify(data)
+
+@app.route('/uploads/<path:filename>', methods=['GET'])
+def download(filename):
+    full_path = os.path.join(app.root_path, 'uploads')
+    return send_from_directory(full_path, filename)
+
+@app.route('/api/autoclick', methods=['GET', 'POST'])
+def autoclick():
+    data = {
+        "code": 200,
+        "message": "Vui long nhap du lieu RUN"
+    }
+    if (request.method == 'POST'):
+        if request.form['state'] != '' and request.form['state'] == 'run':
+            username = request.form['username']
+            password = request.form['password']
+            result = run_autoclick(username, password)
+            data = {
+                "code": 200,
+                "message": result
+            }
+    return jsonify(data)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 5000)))
+    app.run(port=int(os.environ.get("PORT", 7000)))
---
+++ process.py
@@ -0,0 +1,64 @@
+# Author: Nguyen Van Truc
+# Date: 2024/01/20
+
+# Import libraries auch as selenium, time, PIL, pytesseract
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Import libraries for image processing
+from PIL import Image
+import pytesseract
+
+# Import libraries for remove background
+from rembg import remove
+
+# Process image to text
+custom_config = r'--oem 3 --psm 6 -l vie'
+pytesseract.pytesseract.tesseract_cmd = r'D:\App\Tesseract-OCR\tesseract.exe'
+def run_img_to_text(image_path):
+    img = Image.open(image_path)
+    text = pytesseract.image_to_string(img, config=custom_config)
+    return text
+def run_remove_bg(image_path):
+    input = Image.open(image_path)
+    output = remove(input)
+    output.save(image_path, 'PNG')
+
+# Process autoclick program
+def run_autoclick(user, ps):
+    global result
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument(r"executable_path=E:\\3.LAPTRINH\\PYTHON\\chromedriver-win64\\chromedriver.exe")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("http://daotao.ute.udn.vn/viewsmsg.asp")
+    driver.implicitly_wait(10)
+
+    username = driver.find_element(By.NAME, "maSV")
+    username.send_keys(user)
+
+    password = driver.find_element(By.NAME, "pw")
+    password.send_keys(ps)
+
+    btn = driver.find_element(By.XPATH, "//input[@type='submit' and @value='Đăng nhập']")
+    btn.click()
+    time.sleep(3)
+
+    val = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div/div/div[2]/table/tbody/tr[2]/td")
+    # print(val.text)
+    valText = val.text
+    start_index = valText.find("Xin chào sinh viên ") + len("Xin chào sinh viên ")
+    end_index = valText.find("\n", start_index)
+    student_name = valText[start_index:end_index]
+
+    # Print the extracted name
+    # print(student_name)
+
+    driver.close()
+    driver.quit()
+    return student_name
+
+
+if __name__ == "__main__":
+    run_remove_bg()
